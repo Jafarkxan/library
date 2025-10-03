103ec4181ae601e9a4a69f8448aaf9326c85006d
Zafarbek Xudoyqulov <xudoyqulov.zafarbek111@gamil.com>
2025-10-03 18:54:38 +0500

    gunicorn ornatildi

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -12,7 +13,7 @@ SECRET_KEY = 'django-insecure-+os=-li)%)zkt@q*z1hy6j=fo!7p=oagbr@19&do7r-n%__8-#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -129,6 +130,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Bu bitta papka (string bo‘lishi kerak)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# Bu esa list bo‘lishi mumkin
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
